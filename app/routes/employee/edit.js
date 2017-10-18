969d5989658f55cd15c1f606034d06d361b55d96
Ian Irving <ian@falsepositives.com>
2017-10-18 19:57:14 -0400

    allow employee-editable to cancel to the right place

--- app/routes/employee/edit.js
+++ app/routes/employee/edit.js
@@ -3,5 +3,28 @@ import Ember from 'ember'
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('employee', params.employee_id)
+  },
+
+  actions: {
+
+    saveEmployee(employee) {
+      var id = employee.get('id')
+      employee.save().then(() => this.transitionTo('employees'))
+    },
+
+    willTransition(transition) {
+      let model = this.controller.get('model')
+      if (model.get('hasDirtyAttributes')) {
+        let confirmation = confirm("Your changes haven't saved yet. Would you like to leave this form?")
+        // use a dialog instead?
+
+        if (confirmation) {
+          model.rollbackAttributes()
+        } else {
+          transition.abort()
+        }
+      }
+    }
   }
+
 })
--- app/routes/employee/new.js
+++ app/routes/employee/new.js
@@ -5,5 +5,27 @@ export default Ember.Route.extend({
     console.log('employee.new model')
     var newRecord = this.store.createRecord('employee')
     return newRecord
+  },
+
+  actions: {
+
+    saveEmployee(employee) {
+      var id = employee.get('id')
+      employee.save().then(() => this.transitionTo('employees'))
+    },
+
+    willTransition(transition) {
+      let model = this.controller.get('model')
+      if (model.get('hasDirtyAttributes')) {
+        let confirmation = confirm("Your changes haven't saved yet. Would you like to leave this form?")
+        // use a dialog instead?
+
+        if (confirmation) {
+          model.rollbackAttributes()
+        } else {
+          transition.abort()
+        }
+      }
+    }
   }
 })
