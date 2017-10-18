e0447372ef2b74fdb0f2086c0d939c1f2ef569b7
Ian Irving <ian@falsepositives.com>
2017-10-18 13:28:49 -0400

    inital model data for employee

--- app/routes/employees.js
+++ app/routes/employees.js
@@ -1,4 +1,19 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Route.extend({
-});
+  model() {
+    return [{
+      firstName: 'John',
+      lastName: 'Smith',
+      email: 'js@bigCo.com',
+      phoneNumber: '555-555-1234',
+      department: 'Sales'
+    }, {
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: 'jd@bigCo.com',
+      phoneNumber: '555-555-1235',
+      department: 'Marketing'
+    }]
+  }
+})
