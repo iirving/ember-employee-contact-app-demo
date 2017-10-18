48770ab57cea0a256dc903bb84d12e60363c0a73
Ian Irving <ian@falsepositives.com>
2017-10-18 15:30:05 -0400

    add and  employee id's and push to store

--- app/routes/employees.js
+++ app/routes/employees.js
@@ -1,19 +1,38 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
+  // store: Ember.inject.service(),
+
   model() {
-    return [{
-      firstName: 'John',
-      lastName: 'Smith',
-      email: 'js@bigCo.com',
-      phoneNumber: '555-555-1234',
-      department: 'Sales'
-    }, {
-      firstName: 'Jane',
-      lastName: 'Doe',
-      email: 'jd@bigCo.com',
-      phoneNumber: '555-555-1235',
-      department: 'Marketing'
-    }]
+    //    let store = this.store
+
+    return this.get('store').push({
+      data: [{
+        id: 1,
+        type: 'employee',
+        attributes: {
+          firstName: 'John',
+          lastName: 'Smith',
+          email: 'js@bigCo.com',
+          phoneNumber: '555-555-1234',
+          department: 'Sales'
+        },
+        relationships: {}
+      }, {
+        id: 2,
+        type: 'employee',
+        attributes: {
+          firstName: 'Jane',
+          lastName: 'Doe',
+          email: 'jd@bigCo.com',
+          phoneNumber: '555-555-1235',
+          department: 'Marketing'
+        },
+        relationships: {}
+
+      }]
+
+    })
   }
+
 })
