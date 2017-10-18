f0da8f2b446aba9c6863ff986ca8d29562679948
Ian Irving <ian@falsepositives.com>
2017-10-18 16:55:50 -0400

    initial employee-detail component

--- app/router.js
+++ app/router.js
@@ -1,14 +1,19 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from 'ember'
+import config from './config/environment'
 
 const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
-});
+})
 
 Router.map(function() {
-  this.route('employees');
-  this.route('settings');
-});
+  this.route('employees')
+  this.route('settings')
+  this.route('employee', {
+    path: '/employee/:employee_id'
+  }, function() {
+    this.route('new')
+  })
+})
 
-export default Router;
+export default Router
--- app/routes/employees.js
+++ app/routes/employees.js
@@ -1,38 +1,8 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
-  // store: Ember.inject.service(),
-
   model() {
-    //    let store = this.store
-
-    return this.get('store').push({
-      data: [{
-        id: 1,
-        type: 'employee',
-        attributes: {
-          firstName: 'John',
-          lastName: 'Smith',
-          email: 'js@bigCo.com',
-          phoneNumber: '555-555-1234',
-          department: 'Sales'
-        },
-        relationships: {}
-      }, {
-        id: 2,
-        type: 'employee',
-        attributes: {
-          firstName: 'Jane',
-          lastName: 'Doe',
-          email: 'jd@bigCo.com',
-          phoneNumber: '555-555-1235',
-          department: 'Marketing'
-        },
-        relationships: {}
-
-      }]
-
-    })
+    return this.get('store').findAll('employee')
   }
 
 })
