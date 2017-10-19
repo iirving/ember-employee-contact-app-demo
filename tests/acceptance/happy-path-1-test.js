2891a87604703b20630c1316821cbb5683fc5bf9
Ian Irving <ian@falsepositives.com>
2017-10-19 10:03:54 -0400

    get all 9 happy path acceptance tests working

--- tests/acceptance/happy-path-1-test.js
+++ tests/acceptance/happy-path-1-test.js
@@ -38,18 +38,77 @@ test('link to home page from another page via the header', function(assert) {
   })
 })
 
-test('from the home page open a contact detail', function(assert) {
-  visit('/')
+test('from the employees page open a contact detail', function(assert) {
+  let employees = server.createList('employees', 3)
+  let e_id = employees[0].id
+
+  visit('/employees')
+  click('tbody tr:first td:nth-child(2) a')
 
   andThen(function() {
-    assert.equal(currentURL(), '/')
+    assert.equal(currentURL(), `/employee/${e_id}`, 'we are on the employee page for id ' + e_id)
+    //    assert.equal(find(['data-test-sub-header-text']).text(), 'Employee Edit', 'we have the right sub header text')
+    // assert.equal(find('tbody tr').length, 3, 'we have the 3 employees')
+    // assert.equal(find('tbody tr:first td:nth-child(2) a').text(), employees[0].firstName, 'first name is in the second table cell')
   })
 })
 
-test('from the home page open a contect detail, edit it and save it', function(assert) {
-  visit('/')
+test('from the employees page open a contact detail and test the back button link', function(assert) {
+  server.createList('employees', 3)
+
+  visit('/employees')
+  click('tbody tr:first td:nth-child(2) a') // go to the first data row and click a a link
+  click('[data-test-sub-header-back-link]') // click on the back link in the sub header
 
   andThen(function() {
-    assert.equal(currentURL(), '/')
+    assert.equal(currentURL(), '/employees', 'we are back on the employees list page')
+  })
+})
+
+test('from the employees page open a detail page, and edit it', function(assert) {
+  let employees = server.createList('employees', 3)
+  let e_id = employees[0].id
+
+  visit('/employees')
+  click('tbody tr:first td:nth-child(2) a') // go to the first data row and click a a link
+  click('[data-test-employee-edit-link]') // click on the edit link
+
+  andThen(function() {
+    assert.equal(currentURL(), `/employee/${e_id}/edit`,
+      'we are on the employee edit page for id ' + e_id)
+  })
+})
+
+test('from the employees page open a detail page, edit it and the cancel it', function(assert) {
+  let employees = server.createList('employees', 3)
+  let e_id = employees[0].id
+
+  visit('/employees')
+  click('tbody tr:first td:nth-child(2) a') // go to the first data row and click a a link
+  click('[data-test-employee-edit-link]') // click on the edit link
+  click('[data-test-cancel-link]') // click on the cancel link
+
+  andThen(function() {
+    assert.equal(currentURL(), `/employee/${e_id}`,
+      'we are on the employee detail page for id ' + e_id)
+  })
+})
+
+test('from the employees page open a detail page, edit it and save it', function(assert) {
+  let employees = server.createList('employees', 3)
+  let e_id = employees[0].id
+
+  visit('/employees')
+  click('tbody tr:first td:nth-child(2) a') // go to the first data row and click a a link
+  click('[data-test-employee-edit-link]') // click on the edit link
+  fillIn('[data-test-first-name]', 'first')
+  fillIn('[data-test-last-name]', 'last')
+  click('[data-test-button-submit]')
+
+  andThen(function() {
+    assert.equal(currentURL(), `/employee/${e_id}`,
+      'we are on the employee details page for id ' + e_id)
+    assert.equal(find('[data-test-employee-detail-full-name]').text(), 'first last',
+      'the edit is now displayed on the detailed page')
   })
 })
