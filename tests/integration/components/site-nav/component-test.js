293a888705c8fcbc453d1b41de448c87a6fb1ed3
Ian Irving <ian@falsepositives.com>
2017-10-19 10:52:53 -0400

    test site-nav component renders as expected

--- tests/integration/components/site-nav/component-test.js
+++ tests/integration/components/site-nav/component-test.js
@@ -1,24 +1,19 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
 
 moduleForComponent('site-nav', 'Integration | Component | site nav', {
   integration: true
-});
+})
 
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{site-nav}}`);
+  this.render(hbs `{{site-nav}}`)
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#site-nav}}
-      template block text
-    {{/site-nav}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+  assert.equal(this.$('[data-test-link-to-employees]').text().trim(), 'Employees', 'Employees nav link is rendered')
+  assert.equal(this.$('[data-test-link-to-settings]').text().trim(), 'Settings', 'Settings nav link is rendered')
+})
