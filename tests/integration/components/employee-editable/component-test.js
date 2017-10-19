aaef846f2908ba040098db8e258a92471329549c
Ian Irving <ian@falsepositives.com>
2017-10-19 17:06:16 -0400

    update the employee-editable component test

--- tests/integration/components/employee-editable/component-test.js
+++ tests/integration/components/employee-editable/component-test.js
@@ -1,24 +1,46 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
 
 moduleForComponent('employee-editable', 'Integration | Component | employee editable', {
   integration: true
-});
+})
 
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{employee-editable}}`);
+  this.render(hbs `{{employee-editable}}`)
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(
+    this.$('[data-test-first-name]').attr('placeholder').trim(),
+    'First Name',
+    'First Name field is rendered '
+  )
 
-  // Template block usage:
-  this.render(hbs`
-    {{#employee-editable}}
-      template block text
-    {{/employee-editable}}
-  `);
+  assert.equal(
+    this.$('[data-test-last-name]').attr('placeholder').trim(),
+    'Last Name',
+    'Last Name field is rendered '
+  )
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+  assert.equal(
+    this.$('[data-test-email]').attr('placeholder').trim(),
+    'Email',
+    'Email field is rendered '
+  )
+
+  assert.equal(
+    this.$('[data-test-phone-number]').attr('placeholder').trim(),
+    'Telephone',
+    'Telephone field is rendered '
+  )
+
+  assert.equal(
+    this.$('[data-test-department]').attr('placeholder').trim(),
+    'Department',
+    'Department field is rendered '
+  )
+})
