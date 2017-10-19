c1aab0b7bce603b8934ecc1013c8235387ac8572
Ian Irving <ian@falsepositives.com>
2017-10-19 10:47:59 -0400

    test sub-header-with-back-link component reders as expected

--- tests/integration/components/sub-header-with-back-link/component-test.js
+++ tests/integration/components/sub-header-with-back-link/component-test.js
@@ -1,24 +1,31 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
 
 moduleForComponent('sub-header-with-back-link', 'Integration | Component | sub header with back link', {
   integration: true
-});
+})
 
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{sub-header-with-back-link}}`);
+  this.render(hbs `{{sub-header-with-back-link}}`)
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$('[data-test-sub-header-back-link]').text().trim(),
+    'Back',
+    'back link is visible')
 
   // Template block usage:
-  this.render(hbs`
+  this.render(hbs `
     {{#sub-header-with-back-link}}
       template block text
     {{/sub-header-with-back-link}}
-  `);
+  `)
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+  assert.equal(this.$('[data-test-sub-header-text]').text().trim(),
+    'template block text',
+    'the sub header text is rendered')
+})
