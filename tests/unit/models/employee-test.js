255c0db8fdbf87325a8af2d8905f75d6e9a5627e
Ian Irving <ian@falsepositives.com>
2017-10-19 10:28:34 -0400

    test the employee model fullname cp works

--- tests/unit/models/employee-test.js
+++ tests/unit/models/employee-test.js
@@ -1,12 +1,19 @@
-import { moduleForModel, test } from 'ember-qunit';
+import {
+  moduleForModel,
+  test
+} from 'ember-qunit'
+import Ember from 'ember'
 
 moduleForModel('employee', 'Unit | Model | employee', {
   // Specify the other units that are required for this test.
   needs: []
-});
+})
 
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
-});
+test('correctly computes fullname', function(assert) {
+  let employee = this.subject()
+
+  Ember.run(() => employee.set('firstName', 'ian'))
+  Ember.run(() => employee.set('lastName', 'irving'))
+
+  assert.equal(employee.get('fullName'), 'ian irving')
+})
